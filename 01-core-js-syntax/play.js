e1f6d1143879a798a0ceba05a79fbb8071959109
PKTseng <jk0910948659@Gmail.com>
2021-01-25 16:12:05 +0800

    Practice var function

--- 01-core-js-syntax/play.js
+++ 01-core-js-syntax/play.js
@@ -1,16 +1,33 @@
-var name = 'Max';
-var age = 29;
-var hasHobbies = true;
+// var userName = "Max";
+// var age = 29;
+// var hasHobbies = true;
 
-function summarizeUser(userName, userAge, userHasHobby) {
+// function summarizeUser(userName, userAge, userHasHobby) {
+//   return (
+//     "Name is " +
+//     userName +
+//     ", age is " +
+//     userAge +
+//     " and the user has hobbies: " +
+//     userHasHobby
+//   );
+// }
+
+// console.log(summarizeUser(userName, age, hasHobbies));
+
+let myName = "ken";
+let age = 22;
+let hasHobbies = true;
+
+function practice(myName, age, hasHobbies) {
   return (
-    'Name is ' +
-    userName +
-    ', age is ' +
-    userAge +
-    ' and the user has hobbies: ' +
-    userHasHobby
+    "Name is " +
+    myName +
+    ", age is " +
+    age +
+    " and the user has hobbies: " +
+    hasHobbies
   );
 }
 
-console.log(summarizeUser(name, age, hasHobbies));
+console.log(practice(myName, age, hasHobbies));
